e8146c3d9b4386ffa43c5da2bbd14e88cb71e31c
Dmitriy19 <radchenko1819@gmail.com>
2017-11-15 20:20:28 +0300

    webpack bug fixe

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+const argv = {
+    verbose:    _.includes(process.argv, '-v') || _.includes(process.argv, '--verbose'),
+    json:       _.includes(process.argv, '--json'),
+    production: _.includes(process.argv, '--production'),
+};
+module.exports = {
+    plugins: [
+        require('autoprefixer')({
+            browsers: [
+                "> 5%",            // https://www.netmarketshare.com/browser-market-share.aspx?qprid=2&qpcustomd=0
+                "last 2 versions", // http://caniuse.com/
+            ]
+        }),
+        require('postcss-url-mapper')(function(url) {
+            return argv.production ? url : url.replace(new RegExp('^/'), 'http://localhost:3000/');
+        })
+    ]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,72 @@
+
+const _ = require('lodash');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const argv = {
+    verbose:    _.includes(process.argv, '-v') || _.includes(process.argv, '--verbose'),
+    json:       _.includes(process.argv, '--json'),
+    production: _.includes(process.argv, '--production'),
+};
+
+
+
 module.exports = {
-	entry: './app.js',
-	output: {
-		path: __dirname+'/dist',
-		filename: 'bundle.js'
-	},
-	 module: {
-        rules: [
-        {
+    cache:   true,
+    devtool: argv.production ? "source-maps" : "eval",
+    entry: './app.js',
+    output: {
+        path: __dirname+'/dist',
+        filename: 'bundle.js',
+        publicPath: "dist",
+        
+    },
+     
+
+
+
+
+     module: {
+
+       loaders: [
+            {
+                test: /\.sass$/,
+                loader: argv.production
+                    ? ExtractTextPlugin.extract('file-loader?style-loader?source-map-loader=1', [
+                        'file-loader?css-loader?source-map-loader=1&importLoaders=1',
+                        'file-loader?postcss-loader?source-map-loader=1',
+                        'file-loader?sass-loader?source-map-loader=1'
+                    ]) : [
+                        'style-loader?source-map-loader=1',
+                        'css-loader?source-map-loader=1&importLoaders=1',
+                        'postcss-loader?source-map-loader=1',
+                        'sass-loader?source-map-loader=1'
+                    ].join('!')
+            },
+        
+
+        /*{
             test: /\.sass$/,
             use: [
-               		"style-loader", // creates style nodes from JS strings
+                    "style-loader", // creates style nodes from JS strings
             
-                	"css-loader", // translates CSS into CommonJS
+                    "css-loader", // translates CSS into CommonJS
             
-                	"sass-loader" // compiles Sass to CSS
+                    "sass-loader" // compiles Sass to CSS
             ]
-        },
+        },*/
 
         {
-        	test: /\.(png|svg|jpg|gif)$/,
+            test: /\.(png|svg|jpg|gif)$/,
             use: [
-               		'file-loader'
+                    'file-loader?name=[name].[ext]&outputPath=images/'
             ]
         },
 
         {
-        	test: /\.(woff|woff2|eot|ttf|otf)$/,
-        	use: [
-           		'file-loader'
-         	]
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+                'file-loader?name=[name].[ext]&outputPath=fonts/'
+            ]
         }
-
-
-
-
-       ]
+        ]
     }
-    
 }
